d1fe1a4b2b4b0e101d10c42f24dcf52c68aa8c94
Tommy Truong <tommytruong203@gmail.com>
2026-03-25 14:12:37 -0700

    Implement basic task management functionality in To-Do App

--- To-Do App.py
+++ To-Do App.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+
+tasks = []
+while True:
+    choice = 0
+
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Exit")
+    
+    choice = int(input("Enter choice"))
+    if choice == 1:
+        tasks.append(str(input("Enter task")))
+    elif choice == 2:
+        print(tasks)
+    elif choice == 3:
+        break
+    else:
+        choice = int(intput("Enter a valid number"))
+
+
+
+
